d0bc059e9a3af88f5d62330a7365dc92ad91c011
AstuteDesign <jbldesigninquiry@gmail.com>
2021-09-03 08:16:54 -0500

    Yes commit secondbranch

--- app.js
+++ app.js
@@ -7,4 +7,4 @@ const server = http.createServer()
 //This is a test addition to this branch and I will merge it to the main
 
 
-//I guess this is open on firstbranch so I can commit push and update on this branch
+//I guess this is open on secondbranch so I can commit push and update on this branch, I am having some legit trouble tho
